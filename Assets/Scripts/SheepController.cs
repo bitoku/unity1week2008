83a8e9cd9b82efb76f8ba6c5ff7392920df7ea03
Ayato Tokubi <tokubi.ayato@gmail.com>
2020-08-10 12:53:31 +0900

    add sheep and factory

---
+++ Assets/Scripts/SheepController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector2 _direction;
+    private float _speed;
+    [SerializeField] private float changeRate;
+    
+    void Start()
+    {
+        _direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        _speed = 0.003f;
+    }
+
+    private void ChangeDirection()
+    {
+        _direction += new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized * changeRate;
+        _direction = _direction.normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeDirection();
+        Vector3 direction3 = _speed * _direction;
+        transform.Translate(direction3);
+        Debug.Log(_direction);
+    }
+}
---
+++ Assets/Scripts/SheepFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepFactory : MonoBehaviour
+{
+    private bool _canFactory;
+    [SerializeField] private float interval;
+    private float _timeElapsed;
+    private GameObject _sheep;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sheep = (GameObject) Resources.Load("Sheep");
+        StartFactory();
+    }
+
+    private void StartFactory()
+    {
+        _canFactory = true;
+        _timeElapsed = 0f;
+    }
+
+    private void StopFactory()
+    {
+        _canFactory = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_canFactory) return;
+        _timeElapsed += Time.deltaTime;
+        if (_timeElapsed > interval)
+        {
+            Instantiate(_sheep, Vector3.zero, Quaternion.identity);
+            _timeElapsed = 0f;
+        }
+    }
+}
