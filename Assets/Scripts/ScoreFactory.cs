724d973239de017671271a9e6815fb01bae142ce
Ayato Tokubi <tokubi.ayato@gmail.com>
2020-08-12 15:28:33 +0900

    add sheep animation

---
+++ Assets/Scripts/ScoreFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreFactory : MonoBehaviour
+{
+    private float _elapsedTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+    }
+}
