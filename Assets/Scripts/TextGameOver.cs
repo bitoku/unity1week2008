83cebfc72593cf342a4e98488e2dd8f71b6ea792
Ayato Tokubi <tokubi.ayato@gmail.com>
2020-08-15 16:23:57 +0900

    gameover textを修正

---
+++ Assets/Scripts/TextGameOver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextGameOver : MonoBehaviour
+{
+    private ScoreFactory _scoreFactory;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _scoreFactory = FindObjectOfType<ScoreFactory>();
+        var scoreMessagesCount = _scoreFactory.ScoreMessagesCount();
+        if(scoreMessagesCount % 2 == 1)
+        {
+            this.gameObject.transform.position = new Vector3(Screen.width / 2, 72 * (scoreMessagesCount + 1) /2 + Screen.height / 2, 0);
+        }
+        else
+        {
+            this.gameObject.transform.position = new Vector3(Screen.width / 2, 72 * (scoreMessagesCount + 2) / 2 - 36 + Screen.height / 2, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
